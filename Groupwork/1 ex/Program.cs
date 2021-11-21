ce540a9ea66036c90ab90ae5324ff8ef33fa640a
Egor <fomin0596@gmail.com>
2021-11-21 11:49:09 +0300

    2 задание

---
+++ Groupwork/1 ex/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите два числа: ");
+int x = int.Parse(Console.ReadLine());
+int y = int.Parse(Console.ReadLine());
+if (x*x==y)
+{
+Console.WriteLine("x является квадратом числа y");
+}
+else 
+{
+Console.WriteLine("x не является квадратом числа y");
+}
