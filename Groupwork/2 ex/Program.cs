f334fe0422e6aa85462c33b35477beabe8e1863f
Egor <fomin0596@gmail.com>
2021-11-28 09:48:27 +0300

    ex2

---
+++ Groupwork/2 ex/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите два числа: ");
+int x = int.Parse(Console.ReadLine());
+int y = int.Parse(Console.ReadLine());
+int max = 0;
+int min = 0;
+if(x>y) 
+{
+    max=x; 
+    min=y;
+}
+else
+ {
+max=y;
+min=x;
+}
+Console.Write("Max= ");
+Console.WriteLine(max);
+Console.Write("Min= ");
+Console.WriteLine(min);
